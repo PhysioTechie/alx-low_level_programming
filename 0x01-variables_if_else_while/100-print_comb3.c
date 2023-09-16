8cb6ea55ea29add5cfb6f5aa56b8c59cc5150b9e
Yhuddie1234 <ymmyworth@gmail.com>
2023-09-16 07:15:19 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - main function that prints all possible different combinations of three digits
- * in ascending order separated by comma followed by space
+ * main - Entry point
  *
- * Return: Always 0
+ * Description: Prints all possible different combinations
+ *              of two digits in ascending order, separated
+ *              by a comma and space, using only putchar.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int i = '0';
-	int j = '0';
+    int i, j;
 
-	while (i <= '9')
-	{
-		while (j <= '9')
-		{
-			if (!(i > j) || i == j)
-			{
-				putchar(i);
-				putchar(j);
-				if (i == '8' && j == '9')
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			j++;
-		}
-		j = '0';
-		i++;
-	}
-	return (0);
+    for (i = 0; i <= 8; i++)
+    {
+        for (j = i + 1; j <= 9; j++)
+        {
+            putchar(i + '0');
+            putchar(j + '0');
+            if (i != 8 || j != 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
 }
+
