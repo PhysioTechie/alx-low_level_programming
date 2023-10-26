a6e02164bbaf867abbcdc9a0999ede59a7a6b094
Yhuddie1234 <ymmyworth@gmail.com>
2023-10-26 12:56:36 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _binary_to_unit - a progress that converts binary numbers
+ * to integers/decimal numbers
+ *
+ * @b: a character pointer to a string containing the
+ * binary numbers
+ * Return: the converted decimal (output)
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
+	}
+	return (num);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,3 @@
+#This is a README.md file on 0x14-bit_manipulation
+
+It contains all the solved task on 0x14-bit_manipulation
